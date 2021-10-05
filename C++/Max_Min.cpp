3d93fdbcdb615849fc844c8afbccad49079265d3
vikramaditya <66563078+vikramadityasinghs@users.noreply.github.com>
2021-10-05 10:36:36 +0530

    Create Max_Min.cpp

---
+++ C++/Max_Min.cpp
@@ -0,0 +1,18 @@
+// CPP program to find maximum and minimum of
+// two numbers without using loop and any
+// condition.
+#include<bits/stdc++.h>
+#include<stdio.h>
+ 
+int main ()
+{
+   int a = 15, b = 20;
+   printf("input A:");
+   scanf("%d",&a);
+   
+   printf("input B:");
+   scanf("%d",&b);
+   printf("max = %d\n", ((a + b) + abs(a - b)) / 2);
+   printf("min = %d", ((a + b) - abs(a - b)) / 2);
+   return 0;
+}
