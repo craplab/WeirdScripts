6480ba6850633aad142444bd4969110fd12c6961
Subham Das <subhamdas461@gmail.com>
2021-10-06 22:57:28 +0530

    changed to new way

--- Javascript/rm_whitespace/rm_whitespace.js
+++ Javascript/rm_whitespace/rm_whitespace.js
@@ -5,7 +5,12 @@
  */
 
 function removeExtraWhiteSpce(sentence) {
-    return sentence.replace(/\s+/g, " ").trim();
+    // return sentence.replace(/\s+/g, " ").trim();
+
+    let _splited = sentence.split(" ");
+    const wordList = _splited.filter((e) => e.length > 0);
+    let str = wordList.join(" ");
+    return str;
 }
 
 removeExtraWhiteSpce("  Hello  JavaScript  ");
