3723bc9c09ab04dfab966e3c3f07c42259db4cfd
Bobin <bobinm3@gmail.com>
2021-10-16 13:06:50 +0530

    created the script

---
+++ Python/random_videos_youtube/Readme.md
@@ -0,0 +1,10 @@
+# Random YT video generator
+
+The code searches for random videos on YouTube which should be logged with a person's gmail id.
+The motive of the script is to break the bubble that the YouTube algorithm forms for any individual.
+Here, bubble refers to the YouTube feeding similar type of content to the user based on his search and watch history.
+
+Program flow :
+
+1. The program automatically makes about 10 search queries about random words.
+2. After every search the program sleeps for 5 seconds for the video to be streamed.
--- Python/random_videos_youtube/main.py
+++ Python/random_videos_youtube/main.py
@@ -3,33 +3,37 @@ import time
 import random
 import urllib.request
 import re
+from random_word import RandomWords
+import requests
 
 
-url = 'https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3D%252Fresults%253Fsearch_query%253Dmyquery&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
+# url = 'https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3D%252Fresults%253Fsearch_query%253Dmyquery&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
+
+#chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+#ascii_char = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+# webbrowser.open(url)
+#webbrowser.get(chrome_path).open(url)
+# time.sleep(5)
 
 
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-
-
-ascii_char = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-webbrowser.get(chrome_path).open(url)
-time.sleep(20)
-
-
-for k in range(5):
+for k in range(10):
     
-    str = ''
-    for i in range(5):
-        t = random.randint(0,61)
-        r = ascii_char[t]
-        str = str + r
+    r = RandomWords()
+    str = r.get_random_word()
+    if str == None: str = "None"
+    str_list = list(str.split(" "))
+    str = '-'.join(str_list)
     
-    html = urllib.request.urlopen('https://www.youtube.com/results?search_query='+str)
+    print("search term = " + str)
+    
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+str)
     video_ids = re.findall(r'watch\?v=(\S{11})', html.read().decode())
     
     link = 'https://www.youtube.com/watch?v=' + video_ids[random.randint(0,5)]
-
+    
+    # webbrowser.open(link)
+    r = requests.get(link)
     print(link)
-    webbrowser.get(chrome_path).open(link)
-    time.sleep(5)
+    time.sleep(3)
